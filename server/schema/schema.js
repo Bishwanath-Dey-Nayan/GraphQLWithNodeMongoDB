71d403536bd89a0fb83176319a38af0e14cca227
unknown <16103022@iubat.edu>
2019-10-13 17:52:35 +0600

    server SetUp

---
+++ server/schema/schema.js
@@ -0,0 +1,35 @@
+const graphql = require('graphql');
+const {GraphQLObjectType,GraphQLString,GraphQLID,GraphQLSchema} =  graphql;
+const _ = require('lodash');
+
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
+const BookType = new GraphQLObjectType({
+name:'Book',
+fields:() =>({
+    id:{type:GraphQLID},
+    name:{type:GraphQLString},
+    genre:{type:GraphQLString}
+})
+})
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQueryType',
+    fields:() =>({
+        book:{
+            type:BookType,
+            args:{id:{type:GraphQLID}},
+            resolve:(parent,args)=>{
+               return _.find(books,{id:args.id})
+            }
+        }
+    })
+})
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
